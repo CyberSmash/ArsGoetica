e2b72b0fa609d9a8d4e78cfe85b88300bd599f83
Jordan Sebastian <jordan.d.sebastian@gmail.com>
2026-08-18 22:03:20 -0400

    Recommiting maps and assets to remove path issues.

--- Kenny1-bit.tsx
+++ Kenny1-bit.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.2" name="Kenny1-bit" tilewidth="16" tileheight="16" tilecount="1078" columns="49">
  <tileoffset x="1" y="1"/>
- <image source="../Downloads/kenney_1-bit-pack/Tilesheet/monochrome_packed.png" width="784" height="352"/>
+ <image source="assets/monochrome_packed.png" width="784" height="352"/>
  <tile id="0">
   <properties>
    <property name="material_name" value="dirt"/>
